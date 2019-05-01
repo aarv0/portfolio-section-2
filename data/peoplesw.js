12366ab25010876ea66854981aa53b02de2b3096
Ali <calicorn00@gmail.com>
2019-05-01 14:21:20 -0600

    Update

---
+++ data/peoplesw.js
@@ -0,0 +1,47 @@
+import { people } from '../data/people.js'
+import { planets } from '../data/planets.js'
+
+const getLastNumber = (url) => {
+    let end = url.lastIndexOf('/')
+    let start = end -2
+    if(url.charAt(start) === '/') {
+        start++
+    }
+    return url.slice(start, end)    
+}
+
+const allHomeWorlds = people.map(person => {
+    let foundWorld = planets.find(planet => {
+        return planet.url === person.homeworld
+    })
+    let imageURL = getLastNumber(person.url)
+    return {
+        name: person.name,
+        home: foundWorld.name,
+        eye_color: person.eye_color,
+        imagePath: `https://starwars-visualguide.com/assets/img/characters/${imageURL}.jpg`,
+    }
+})
+
+console.log(allHomeWorlds)
+
+const mainContainer = document.createElement('div')
+mainContainer.className = 'container'
+
+allHomeWorlds.forEach((person) => {
+    let personElement = document.createElement('div')
+    let planetElement = document.createElement('p')
+    let imageElement = document.createElement('img')
+    
+
+    personElement.className = 'notification'
+    personElement.textContent = person.name
+    planetElement.textContent = person.home
+    imageElement.src = person.imagePath
+
+    personElement.appendChild(planetElement)
+    personElement.appendChild(imageElement)
+    mainContainer.appendChild(personElement)
+})
+
+document.body.appendChild(mainContainer)
